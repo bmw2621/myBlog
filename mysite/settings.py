4df85128fb73ee8746d8bf10975d259831f77c56
Benjamin Winchester <benjamin.m.winchester@googlemail.com>
2019-10-28 16:59:48 -0400

    Fix static file reference and blog responsiveness

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,10 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 STATICFILES_DIRS = [
-    "home/static",
-    "blog/static",
-    "resume/static",
+    os.path.join(BASE_DIR, "static"),
 ]
